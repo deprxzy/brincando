5adb7bb5f494e42a5255fd2eb1ce9ec83894cfae
Bernardo <cauabarbosa0405@gmail.com>
2025-09-29 16:02:22 -0300

    remoção de pastas que coloquei sem querer

--- exe04.py
+++
@@ -1,9 +0,0 @@
-estoque = {
-    
-}
-for i in range(3):
-    produto = input("Digite o nome do produto: ")
-    quantidade = int(input("Digite a quantidade: "))
-    estoque[produto] = quantidade
-    
-print(estoque)
